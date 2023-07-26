f182d2242ba28aecf4a0086efdf09fbbcbc80bdd
rcshpark <rcshpark@gmail.com>
2023-07-26 22:32:38 +0900

    feat: 모든 질문 가져오기

---
+++ lib/provider/question_provider.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:copum_front_update/model/question_model.dart';
+import "package:http/http.dart" as http;
+import 'package:flutter/material.dart';
+
+String questionUrl = Platform.isAndroid
+    ? 'http://10.0.2.2:8000/question/list/{category}'
+    : 'http://127.0.0.1:8000/question/list/{category}';
+
+class QuestionProvider with ChangeNotifier {
+  QuestionModel questionModel = QuestionModel();
+  fetchData() async {
+    try {
+      http.Response response = await http.get(Uri.parse(questionUrl));
+      dynamic body = jsonDecode(utf8.decode(response.bodyBytes));
+      questionModel = QuestionModel.fromJson(body);
+    } catch (e) {
+      return e;
+    }
+  }
+}
