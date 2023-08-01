97a8b3ce5cfb03640d654fad931b8da9d9f51f42
rcshpark <rcshpark@gmail.com>
2023-08-01 22:17:11 +0900

    feat: 질문 클릭 이벤트

--- lib/page/home_detail/new_question.dart
+++ lib/page/home_detail/new_question.dart
@@ -1,8 +1,8 @@
-import 'package:copum_front_update/model/question_model.dart';
+import 'package:copum_front_update/page/home_detail/question_detail_page.dart';
 import 'package:copum_front_update/provider/question_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 class NewQuestionPage extends StatefulWidget {
   const NewQuestionPage({super.key});
@@ -23,12 +23,42 @@ class _NewQuestionPageState extends State<NewQuestionPage> {
                       itemBuilder: (context, index) {
                         return Column(
                           children: [
-                            questionCard(
+                            InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            QuestionDetailPage(
+                                                nickname: p.questionModel
+                                                    .result![index].creator,
+                                                createdAt: p.questionModel
+                                                    .result![index].createAt,
+                                                title: p.questionModel
+                                                    .result![index].title,
+                                                imageURL: p
+                                                    .questionModel
+                                                    .result![index]
+                                                    .questionImage,
+                                                content: p.questionModel
+                                                    .result![index].content,
+                                                viewCount: p.questionModel
+                                                    .result![index].viewCount,
+                                                answerCount: p
+                                                    .questionModel
+                                                    .result![index]
+                                                    .answerCount)));
+                              },
+                              child: questionCard(
                                 p.questionModel.result![index].creator,
                                 p.questionModel.result![index].createAt,
                                 p.questionModel.result![index].title,
                                 p.questionModel.result![index].questionImage,
-                                p.questionModel.result![index].content)
+                                p.questionModel.result![index].content,
+                                p.questionModel.result![index].viewCount,
+                                p.questionModel.result![index].answerCount,
+                              ),
+                            )
                           ],
                         );
                       },
@@ -48,13 +78,14 @@ class _NewQuestionPageState extends State<NewQuestionPage> {
 }
 
 Widget questionCard(String? nickname, String? createdAt, String? title,
-    String? imageURL, String? content) {
+    String? imageURL, String? content, String? viewCount, int? answerCount) {
+  DateTime dateTime = DateTime.parse(createdAt!);
   return Card(
     color: Colors.black,
     elevation: 4.0,
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
     child: Padding(
-      padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.only(left: 20, top: 20),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -74,7 +105,7 @@ Widget questionCard(String? nickname, String? createdAt, String? title,
                 width: 10,
               ),
               Text(
-                '$nickname\n$createdAt',
+                '$nickname\n${DateFormat.yMMMd().format(dateTime)}',
                 style: const TextStyle(color: Colors.white),
               )
             ],
@@ -87,6 +118,9 @@ Widget questionCard(String? nickname, String? createdAt, String? title,
             style: const TextStyle(color: Colors.white, fontSize: 20),
             maxLines: 3,
           ),
+          const SizedBox(
+            height: 5,
+          ),
           imageURL!.isEmpty
               ? const SizedBox()
               : Container(
@@ -94,9 +128,19 @@ Widget questionCard(String? nickname, String? createdAt, String? title,
                   width: 40,
                   height: 40,
                 ),
+          const SizedBox(
+            height: 5,
+          ),
           Text(
             content!,
             style: const TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            '$viewCount 번의 조회 $answerCount 개의 답변',
+            style: const TextStyle(color: Colors.grey, fontSize: 14),
           )
         ],
       ),
