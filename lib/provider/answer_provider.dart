f31f712ba290a1164026a48d09dc4f0b552a44f0
rcshpark <rcshpark@gmail.com>
2023-08-13 22:39:45 +0900

    feat: 해당 질문에 대한 답변 생성 로직

--- lib/provider/answer_provider.dart
+++ lib/provider/answer_provider.dart
@@ -5,6 +5,10 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+String answerURL = Platform.isAndroid
+    ? 'http://10.0.2.2:8000/answer'
+    : 'http://127.0.0.1:8000/answer';
+
 class AnswerProvider with ChangeNotifier {
   // AnswerModel answerModel = AnswerModel();
   AnswerModel answerModel = AnswerModel();
@@ -19,6 +23,31 @@ class AnswerProvider with ChangeNotifier {
       answerModel = AnswerModel.fromJson(body);
       print("####");
       notifyListeners();
+    } catch (e) {
+      return e;
+    }
+  }
+
+  insertAnswer(String? questionId, String? creator, String? content,
+      String? image) async {
+    try {
+      dynamic data = {
+        "QUESTION_ID": questionId,
+        "CONTENT": content,
+        "CREATOR": creator,
+        "ANSWER_IMAGE": image,
+      };
+      final String jsonString = jsonEncode(data);
+      http.Response response = await http.post(Uri.parse("$answerURL/create"),
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: jsonString);
+      dynamic body = jsonDecode(utf8.decode(response.bodyBytes));
+      notifyListeners();
+      if (body['status_code'] == 200) {
+        return "success";
+      }
     } catch (e) {
       return e;
     }
