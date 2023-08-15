c7078fd185972e6b5e3e32ceffd47f9fbaecca21
rcshpark <rcshpark@gmail.com>
2023-08-15 20:09:49 +0900

    user model 수정

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,21 +1,23 @@
 class UserModel {
-  final int id;
-  final String email;
-  final String nickname;
-  final String profileImage;
+  int? id;
+  String? email;
+  String? nickname;
+  String? profileImage;
 
-  UserModel(this.id, this.email, this.nickname, this.profileImage);
+  UserModel({this.id, this.email, this.nickname, this.profileImage});
 
-  UserModel.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        email = json['email'],
-        nickname = json['nickname'],
-        profileImage = json['profileImage'];
-
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'email': email,
-        'nickname': nickname,
-        'profileImage': profileImage
-      };
+  UserModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    email = json['email'];
+    nickname = json['nickname'];
+    profileImage = json['profileImage'];
+  }
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['email'] = email;
+    data['nickname'] = nickname;
+    data['profileImage'] = profileImage;
+    return data;
+  }
 }
